be8d1669af1c323a52df1b73aa2bbe8416a8594f
Vicky <vickymenashe@gmail.com>
2022-10-30 18:29:27 +0200

    copyToClip function

---
+++ .history/script_20221030182716.js
@@ -0,0 +1,3 @@
+function copyToClip(el) {
+  console.log('copied!')
+}
---
+++ copy-to-clip.js
@@ -0,0 +1,19 @@
+function copyToClip(el) {
+    /* Get the text field */
+    var copyText = el.children[0];
+
+    // /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+    // /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    // /* Alert the copied text */
+    var notify = document.getElementById("notify");
+    notify.classList.toggle('show')
+
+    setTimeout(function () {
+        notify.classList.toggle('show')
+    }, 2000)
+}
