fed4d88a82179fbc8210a4401537bb608e3d1613
Vicky <vickymenashe@gmail.com>
2023-07-08 20:46:54 +0300

    reset proj - CaMar23

--- copy-to-clip.js
+++ copy-to-clip.js
@@ -1,21 +1,4 @@
 
 function copyToClip(el) {
     console.log('copied!')
-    /* Get the text field */
-    var copyText = el.children[0];
-
-    // /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-    // /* Copy the text inside the text field */
-    document.execCommand("copy");
-
-    // /* Alert the copied text */
-    var notify = document.getElementById("notify");
-    notify.classList.toggle('show')
-
-    setTimeout(function () {
-        notify.classList.toggle('show')
-    }, 2000)
 }
